8059a1aa19b776b4ebbb52919d9eaf13d98ad8cc
subham <shubham.gaurav.1994@gmail.com>
2017-02-07 06:51:55 +0530

    Add undirected edge weighted graph

--- Graphs/graph_builder.py
+++ Graphs/graph_builder.py
@@ -50,3 +50,22 @@ def build_undirected_graph():
 	graph.add_edge('w', 'z', 'undirected')
 	graph.add_edge('z', 'z', 'undirected')
 	return graph, graph.get_vertex('u')
+
+
+def build_undirected_edge_weighted_graph():
+	graph = Graph()
+	graph.add_edge('a', 'b', cost=4, edge_type='undirected')
+	graph.add_edge('b', 'c', cost=8, edge_type='undirected')
+	graph.add_edge('c', 'd', cost=7, edge_type='undirected')
+	graph.add_edge('d', 'e', cost=9, edge_type='undirected')
+	graph.add_edge('e', 'f', cost=10, edge_type='undirected')
+	graph.add_edge('f', 'g', cost=2, edge_type='undirected')
+	graph.add_edge('g', 'h', cost=1, edge_type='undirected')
+	graph.add_edge('a', 'h', cost=8, edge_type='undirected')
+	graph.add_edge('b', 'h', cost=11, edge_type='undirected')
+	graph.add_edge('h', 'i', cost=7, edge_type='undirected')
+	graph.add_edge('i', 'c', cost=2, edge_type='undirected')
+	graph.add_edge('i', 'g', cost=6, edge_type='undirected')
+	graph.add_edge('c', 'f', cost=4, edge_type='undirected')
+	graph.add_edge('d', 'f', cost=14, edge_type='undirected')
+	return graph, graph.get_vertex('a')
